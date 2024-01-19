bc9e02e60d750d474604fe631173f3ba23cd91cc
Pol Valero <pol.valero.arjona@gmail.com>
2024-01-19 13:32:27 +0100

    getOtherUsers() and getUserDataFromUID() functions implemented

---
+++ lib/entities/user_data.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class UserData {
+   late String uid;
+   late Image profileImage;
+   late String name;
+   late String age;
+   late String interestsField;
+   late String aboutMeField;
+}
--- lib/views/tab_chat.dart
+++ lib/views/tab_chat.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+
+import '../entities/user_data.dart';
 
 class TabChatView extends StatefulWidget {
   const TabChatView({Key? key}) : super(key: key);
@@ -10,14 +14,15 @@ class TabChatView extends StatefulWidget {
 
 class _TabChatViewState extends State<TabChatView> {
 
-  String userMail = '';
+  late User loggedUser;
+  late List<UserData> otherUsers;
 
   @override
   void initState() {
     super.initState();
 
     getUserMail().then((value) {
-
+        getOtherUsers(loggedUser);
         setState(() {});
     });
 
@@ -25,20 +30,58 @@ class _TabChatViewState extends State<TabChatView> {
 
   //Function to get the mail of the current logged user
   Future getUserMail() async {
-    final user = FirebaseAuth.instance.currentUser;
-    userMail = user!.email!;
+    loggedUser = FirebaseAuth.instance.currentUser!;
+    //userMail = user!.email!;
     setState(() {
     });
   }
 
-  //TODO: Function getOtherUsers() to get the list of other users
+  Future getOtherUsers(User loggedUser) async {
+    final users = await FirebaseFirestore.instance.collection('users').get();
+    for (var user in users.docs) {
+      if (user.id != loggedUser.uid) {
+
+        //We can get the user data
+        //print(user.data()['name']);
+
+        await getUserDataFromUID(user.id).then((value) {
+          otherUsers.add(value);
+        });
+      }
+    }
+  }
+
+  Future<UserData> getUserDataFromUID(String uid) async {
+
+    UserData userData = UserData();
+    await FirebaseFirestore.instance.collection('users').doc(uid).get().then((value) async {
+      userData.uid = uid;
+      userData.name = value.get('name');
+      //TODO: UNCOMMENT
+      /*var birthdate = value.get('birthdate');
+      userData.age = (DateTime
+          .now()
+          .difference(DateTime.parse(birthdate))
+          .inDays / 365).floor().toString();
+      userData.interestsField = value.get('interests');
+      userData.aboutMeField = value.get('about_me');
+
+      await FirebaseStorage.instance.ref().child('profile_images/$uid').getDownloadURL().then((value) async {
+        userData.profileImage = Image.network(value, height: 200, width: 200);
+      });
+      */
+      return userData;
+    });
+
+    return userData;
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(userMail),
+        child: Text(loggedUser.email!)
       ),
     );
   }
